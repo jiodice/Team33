75c33fa840a9f9ee27519c74a6c69f6c5cb72d6f
Joey Iodice <joeyiodice@gmail.com>
2018-04-17 11:38:41 -0500

    Device Setup

---
+++ SimulatedDevice/DeviceIdentity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+//using Telemetry;
+
+namespace SimulatedDevice
+{
+    class DeviceIdentity
+    {
+        private static RegistryManager _registryManager;
+        private const string ConnectionString = "HostName=Team33Hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=jOMoVfLXXRadyke4fHP1M7K71PO/k21vYDFy+NDWnCU=";
+        private const string Name = "Team 33 Device ?";
+        private const string TelemetryKey = "telemetry";
+        private const string InstrumentationKey = "instrumentationKey";
+        //private static Telemetry TelemetryClient;
+        private static readonly Configuration Config = ConfigurationManager.OpenExeConfiguration(System.IO.Path.Combine(
+            Environment.CurrentDirectory, System.Reflection.Assembly.GetExecutingAssembly().ManifestModule.Name));
+
+        public static async Task<string> SetupDeviceAsync(string deviceId)
+        {
+            Device device;
+            try
+            {
+                device = await _registryManager.AddDeviceAsync(new Device(deviceId));
+                SendTelemetry("success", "register new device");
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await _registryManager.GetDeviceAsync(deviceId);
+                SendTelemetry("success", "device existed");
+            }
+            catch (Exception e)
+            {
+                SendTelemetry("failed", $"register device failed: {e.Message}");
+                Console.WriteLine($"register device failed: {e.Message}");
+                throw;
+            }
+
+            Console.WriteLine($"device key : {device.Authentication.SymmetricKey.PrimaryKey}");
+
+            return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+
+        private static void SendTelemetry(string eventName, string message)
+        {
+            //if (TelemetryClient != null)
+            //{
+            //    bool shouldSend;
+            //    bool.TryParse(Config.AppSettings.Settings[TelemetryKey].Value, out shouldSend);
+            //    if (shouldSend)
+            //    {
+            //        TelemetryClient.Track(eventName, ConnectionString, Name, message);
+            //    }
+            //}
+        }
+    }
+}
--- SimulatedDevice/Program.cs
+++ SimulatedDevice/Program.cs
@@ -8,10 +8,15 @@ namespace SimulatedDevice
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        private const string DeviceId = "Team33-3";
+
+        static async Task MainAsync(string[] args)
         {
-            //hello
+            //Setup device in Azure
+            string deviceId = await DeviceIdentity.SetupDeviceAsync(DeviceId);
             
+
         }
         private void donothing(){
             
